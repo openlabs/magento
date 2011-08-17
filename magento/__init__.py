93d504630ffb1d6275db97888b50336d2d7ba8cd
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-08-17 13:06:45 -0300

    Added sales API to __init__ for easy import from package, Thanks to Jim Karsten

--- magento/__init__.py
+++ magento/__init__.py
@@ -7,7 +7,7 @@
     :copyright: (c) 2010 by Openlabs Technologies & Consulting (P) LTD
     :license: AGPLv3, see LICENSE for more details
 '''
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 __all__ = [
             'API',
@@ -15,7 +15,8 @@ __all__ = [
             'Country', 'Region',
             'Category', 'CategoryAttribute', 'Product', 'ProductAttribute',
             'ProductAttributeSet', 'ProductTypes', 'ProductImages',
-            'ProductTierPrice', 'ProductLinks', 'Inventory'
+            'ProductTierPrice', 'ProductLinks', 'Inventory',
+            'Order', 'Shipment', 'Invoice',
             ]
 
 from api import API
@@ -25,3 +26,4 @@ from catalog import Category, CategoryAttribute
 from catalog import Product, ProductAttribute, ProductAttributeSet
 from catalog import ProductTypes, ProductImages, ProductTierPrice
 from catalog import ProductLinks, Inventory
+from sales import Order, Shipment, Invoice
