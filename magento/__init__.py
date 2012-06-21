a25dcbd360773602354086d56f00fed18e186792
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-06-21 19:30:19 +0530

    Bump version to dev

--- magento/__init__.py
+++ magento/__init__.py
@@ -3,10 +3,10 @@
     magento API
 
     :copyright: (c) 2010 by Sharoon Thomas.
-    :copyright: (c) 2010 by Openlabs Technologies & Consulting (P) LTD
+    :copyright: (c) 2010-2012 by Openlabs Technologies & Consulting (P) LTD
     :license: AGPLv3, see LICENSE for more details
 '''
-__version__ = '0.1.4'
+__version__ = '0.1.5dev'
 
 __all__ = [
             'API',
